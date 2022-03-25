37a118400e3a17eafcf143ce230aebed9734feda
Patriciaahp <patrihernandezperez9@gmail.com>
2022-03-25 08:40:31 +0100

    Corregido errores, añadido ResponseCodeUserDeleted y creada clase DeleteUserAction

--- Domain/Users/Users/ResponseCodes/ResponseCodeUserDeleted.php
+++ Domain/Users/Users/ResponseCodes/ResponseCodeUserDeleted.php
@@ -1,17 +1,16 @@
 <?php
 
 namespace Domain\Users\Users\ResponseCodes;
-
 use Domain\Shared\ActionResponseCode;
-
-class ResponseCodeUserUpdated extends ActionResponseCode
+class ResponseCodeUserDeleted extends ActionResponseCode
 {
-    function __construct($object){
+    function __construct()
+    {
         parent::__construct(
             204,
             ActionResponseCode::STATUS_SUCCESS,
-            'User updated',
-            $object
+            'User deleted',
         );
     }
+
 }
